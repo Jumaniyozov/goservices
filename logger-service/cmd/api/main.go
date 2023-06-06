53ec1645d3cfef5b1f1aed47d6aa803b468b342b
Jumaniyozov <jumaniyozovse@gmail.com>
2023-06-06 15:56:26 +0500

    other services such grpc

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -26,16 +28,18 @@ type Config struct {
 }
 
 func main() {
-
+	// connect to mongo
 	mongoClient, err := connectToMongo()
 	if err != nil {
 		log.Panic(err)
 	}
 	client = mongoClient
 
+	// create a context in order to disconnect
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
+	// close connection
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
@@ -46,6 +50,9 @@ func main() {
 		Models: data.New(client),
 	}
 
+	go app.rpcListen()
+	
+	// start web server
 	log.Println("Starting service on port", webPort)
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -59,14 +66,33 @@ func main() {
 
 }
 
-func connectToMongo() (*mongo.Client, error) {
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
 
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+	}
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
 	clientOptions := options.Client().ApplyURI(mongoURL)
 	clientOptions.SetAuth(options.Credential{
 		Username: "admin",
 		Password: "password",
 	})
 
+	// connect
 	c, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Println("Error connecting:", err)
